ef7f62641fa1a141adccf93e293ad407eff81a29
Soren Schultz <sschultz@magicmusicapp.com>
2026-02-07 15:40:26 -0500

    add mouse responsiveness to animation

--- src/App.tsx
+++ src/App.tsx
@@ -9,46 +9,45 @@ import MNIST from './pages/MNIST'
 
 import AsciiAnimation from './components/AsciiAnimation'
 import { useRef } from 'react'
-import { Box, useMediaQuery } from '@mui/material'
+import { Box } from '@mui/material'
+import { useMediaQuery } from '@mui/material'
 
 function App() {
-    const isMobile = useMediaQuery('(max-width: 600px)');
     const asciiContainerRef = useRef<HTMLDivElement>(null);
+    const isMobile = useMediaQuery('(max-width: 600px)');
     return (
-        <>
-            <ThemeContextProvider>
-                <Box
-                    ref={asciiContainerRef}
-                    sx={{
-                        position: 'fixed',
-                        top: { xs: 0, sm: 0, md: '-150px' },
-                        right: { xs: 0, sm: 0, md: '-150px' },
-                        width: { xs: '400px', sm: '500px', md: '900px' },
-                        height: { xs: '400px', sm: '500px', md: '900px' },
-                        zIndex: 0,
-                        opacity: 0.3,
-                        pointerEvents: 'none',
-                        overflow: 'visible',
-                        '@media print': { display: 'none' },
-                        fontWeight: 'bold',
-                    }}
-                >
-                    <AsciiAnimation containerRef={asciiContainerRef} resolution={isMobile ? 0.4 : 0.3} />
-                </Box>
-                <BrowserRouter>
+        <ThemeContextProvider>
+            <Box
+                ref={asciiContainerRef}
+                sx={{
+                    position: 'fixed',
+                    top: { md: -150 },
+                    right: { xs: -100, md: -150 },
+                    width: { xs: '400px', sm: '500px', md: '900px' },
+                    height: { xs: '400px', sm: '500px', md: '900px' },
+                    zIndex: 0,
+                    opacity: 0.3,
+                    pointerEvents: 'none',
+                    overflow: 'visible',
+                    '@media print': { display: 'none' },
+                    fontWeight: 'bold',
+                }}
+            >
+                <AsciiAnimation containerRef={asciiContainerRef} resolution={isMobile ? 0.4 : 0.3} />
+            </Box>
+            <BrowserRouter>
 
-                    <Navbar />
-                    <Routes>
-                        <Route path="/resume" element={<Resume />} />
-                        <Route path="/projects" element={<Projects />} />
-                        <Route path="/artwork" element={<Artwork />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="/mnist" element={<MNIST />} />
-                        <Route path="/" element={<Navigate to="/resume" />} />
-                    </Routes>
-                </BrowserRouter>
-            </ThemeContextProvider>
-        </>
+                <Navbar />
+                <Routes>
+                    <Route path="/resume" element={<Resume />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/artwork" element={<Artwork />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/mnist" element={<MNIST />} />
+                    <Route path="/" element={<Navigate to="/resume" />} />
+                </Routes>
+            </BrowserRouter>
+        </ThemeContextProvider>
     )
 }
 
